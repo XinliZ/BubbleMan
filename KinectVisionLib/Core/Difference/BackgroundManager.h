bf300af41fc1ced821ca61b0059a0db238e19015
Xinli Zou <xlzou@live.com>
2016-05-03 14:04:12 -0700

    Created the ImageOperation function

--- KinectVisionLib/Core/Difference/BackgroundManager.h
+++ KinectVisionLib/Core/Difference/BackgroundManager.h
@@ -14,6 +14,7 @@ namespace KinectVisionLib
             BackgroundManager(shared_ptr<DepthImage> initialImage)
             {
                 this->background = make_shared<DepthImage>(*initialImage.get());
+				this->backgroundAccumulationMask = make_shared<DepthImage>(*initialImage.get());
             }
 
             shared_ptr<DepthImage> Update(shared_ptr<DepthImage> image)
@@ -22,43 +23,31 @@ namespace KinectVisionLib
             }
 
         private:
-            shared_ptr<DepthImage> SubstractAndUpdateBackground(shared_ptr<DepthImage> image)
-            {
-                double backgroundThreshold = 1.05;        // TODO: Should make this a configuration value
-                shared_ptr<DepthImage> result = make_shared<DepthImage>(image->GetWidth(), image->GetHeight());
-                for (int i = 0; i < image->GetHeight(); i++)
-                {
-                    uint16* bgScan = this->background->GetScan0() + i * this->background->GetStride();
-                    uint16* imageScan = image->GetScan0() + i * image->GetStride();
-                    uint16* resultScan = result->GetScan0() + i * result->GetStride();
-                    for (int j = 0; j < image->GetWidth(); j++)
-                    {
-                        // TODO: We would need a gaussian model to tell if the point is a background point
-                        if (*imageScan == 0)
-                        {
-                            // Ignore invalid input point
-                        }
-                        else if (*bgScan == 0 || *imageScan > *bgScan)
-                        {
-                            // The new background exposed, update the background only
-                            *bgScan = *imageScan;
-                            *resultScan = 10;
-                        }
-                        else if ((*imageScan) * backgroundThreshold < *bgScan)
-                        {
-                            *resultScan = *imageScan;
-                        }
-
-                        bgScan++;
-                        imageScan++;
-                        resultScan++;
-                    }
-                }
-                return result;
-            }
+			shared_ptr<DepthImage> SubstractAndUpdateBackground(shared_ptr<DepthImage> image)
+			{
+				double backgroundThreshold = 1.05;        // TODO: Should make this a configuration value
+				shared_ptr<DepthImage> result = make_shared<DepthImage>(image->GetWidth(), image->GetHeight());
+				background->ImageOperation<uint16, uint16>(image, result, [backgroundThreshold](uint16* bgScan, uint16* inputScan, uint16* resultScan) {
+					if (*inputScan == 0)
+					{
+						// Ignore invalid input point
+					}
+					else if (*bgScan == 0 || *inputScan > *bgScan)
+					{
+						*bgScan = *inputScan;
+						*resultScan = 10;
+					}
+					else if ((*inputScan) * backgroundThreshold < *bgScan)
+					{
+						*resultScan = *bgScan;
+					}
+				});
+				return result;
+			}
 
         private:
             shared_ptr<DepthImage> background;
+			shared_ptr<DepthImage> backgroundAccumulationMask;
         };
     }
 }
